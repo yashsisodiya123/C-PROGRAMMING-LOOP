6ec031f2cb582560b012b1be79a2b8ad570e2eea
yashsisodiya123 <sisodiyay35@gmail.com>
2025-09-15 02:20:54 +0530

    Update how many prime no. are there between 1 to 500.c

--- how many prime no. are there between 1 to 500.c
+++ how many prime no. are there between 1 to 500.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 int main(){
-int i,j,N,sum,prime_numbers;
+int i,j,N,sum,total;
 
-printf("no. of prime numbers is= ");
-
-prime_numbers=0;
+total=0;
 
 for(i=2;i<=500;i=i+1)
 {
@@ -12,13 +10,12 @@ for(i=2;i<=500;i=i+1)
 
 for(j=1;j<=i;j=j+1)
 {
-
-    if(i%j==0)
-    sum=sum+j;
+    if(i%j==0){
+    sum=sum+j;}
 }
-    if(sum==1+i)
-    prime_numbers=prime_numbers+1;
+    if(sum==i+1){
+    total=total+1;}
 }
 
-    printf("%d", prime_numbers);
+ printf("number of prime numbers from 1 to 500= %d", total);
 }
