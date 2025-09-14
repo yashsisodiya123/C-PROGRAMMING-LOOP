72b5028de80523a1a3450d330ead10b999c9d3a9
yashsisodiya123 <sisodiyay35@gmail.com>
2025-09-15 02:13:07 +0530

    Update prime no. between 1 to 500.c

--- prime no. between 1 to 500.c
+++ prime no. between 1 to 500.c
@@ -1,17 +1,21 @@
 #include<stdio.h>
 int main(){
-int i,j,N,sum;
+int i,j,N,sum,total;
 
-printf("prime numbers are= \n");
+total=0;
 
 for(i=2;i<=500;i=i+1)
 {
     sum=0;
+
 for(j=1;j<=i;j=j+1)
 {
-    if(i%j==0)
-    sum=sum+j;
+    if(i%j==0){
+    sum=sum+j;}
 }
     if(sum==i+1){
-    printf("%d\n",i);
-}}}
+    total=total+i;}
+}
+
+ printf("sum of prime numbers from 1 to 500= %d", total);
+}
