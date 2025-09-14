c4b026d921e80483419ac78c355e726a42d49a2d
yashsisodiya123 <sisodiyay35@gmail.com>
2025-09-15 01:53:53 +0530

    Update prime no. between 1 to 500.c

--- prime no. between 1 to 500.c
+++ prime no. between 1 to 500.c
@@ -7,13 +7,11 @@ printf("prime numbers are= \n");
 for(i=2;i<=500;i=i+1)
 {
     sum=0;
-
 for(j=1;j<=i;j=j+1)
 {
-
     if(i%j==0)
     sum=sum+j;
-
-    if(sum==i+1)
+}
+    if(sum==i+1){
     printf("%d\n",i);
 }}}
