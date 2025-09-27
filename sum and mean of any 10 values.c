800ad815673ef4e7cfb5f51cafa9a1a7756248f1
yashsisodiya123 <sisodiyay35@gmail.com>
2025-09-27 21:34:36 +0530

    Update sum and mean of any 10 values.c

--- sum and mean of any 10 values.c
+++ sum and mean of any 10 values.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
 
 int main(){
-int i,num,sum,mean;
+float i,num,sum,mean;
 sum=0;
 
 for(i=1;i<=10;i=i+1)
 {
 
-printf("enter number %d= ", i);
-scanf("%d", &num);
+printf("enter number %.f= ", i);
+scanf("%f", &num);
 
 sum=sum+num;
 
 }
 mean=sum/10;
-printf("sum= %d\n", sum);
-printf("mean= %d\n", mean);
+printf("sum= %.2f\n", sum);
+printf("mean= %.2f\n", mean);
 
 }
+
 
